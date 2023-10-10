b1636394277ed9f9e1ff2e989254de636f2dbad8
Sharmake Awil <sharmarke.awil@hotmail.com>
2023-10-10 23:20:38 +0200

    pokemond condition

--- myreact/src/components/PokemonCard.jsx
+++ myreact/src/components/PokemonCard.jsx
@@ -1,13 +1,32 @@
-
-
-
-function PokemonCard(){
-
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
+  function PokemonCard() {
+    const pokemonIndex = 0; 
+    const pokemon = pokemonList[pokemonIndex];
+  
     return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        </figure>
-    )
-}
+      <figure>
+        {pokemon ? (<> <img src={pokemon.imgSrc} alt={pokemon.name} />  <figcaption>{pokemon.name}</figcaption> </>  ) : (
+          <p>???</p>
+        )}
+      </figure>
+    );
+  }
+  
+  
+  
+
+
+
+
+  
 
 export default PokemonCard;
